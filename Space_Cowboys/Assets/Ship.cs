2a0cc2bd261bf885401a1cc8fdf258d8b45ba3c5
Takács Marcell <tmarcell20012001@gmail.com>
2024-12-02 21:43:49 +0100

    Hajo mozgasa / palyahatarok

--- Space_Cowboys/Assets/Ship.cs
+++ Space_Cowboys/Assets/Ship.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
+
+    float moveSpeed = 3;
+    bool moveUp;
+    bool moveDown;
+    bool moveLeft;
+    bool moveRight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,58 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveUp = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+        moveDown = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+        moveLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        moveRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+    }
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+        float moveAmount = moveSpeed * Time.fixedDeltaTime;
+        Vector2 move = Vector2.zero;
+        if (moveUp)
+        {
+            move.y += moveAmount;
+        }
+        if (moveDown)
+        {
+            move.y -= moveAmount;
+        }
+        if (moveLeft)
+        {
+            move.x -= moveAmount;
+        }
+        if (moveRight)
+        {
+            move.x += moveAmount;
+        }
+
+        float moveMagnitude = Mathf.Sqrt(move.x * move.x + move.y * move.y);
+        if (moveMagnitude > moveAmount)
+        {
+            float ratio = moveAmount / moveMagnitude;
+            move *= ratio;
+        }
+
+        pos += move;
+        if (pos.x <= 0.5f)
+        {
+            pos.x = 0.5f;
+        }
+        if (pos.x >= 15f)
+        {
+            pos.x = 15;
+        }
+        if (pos.y <= -4f)
+        {
+            pos.y = -4f;
+        }
+        if (pos.y >= 4.5f)
+        {
+            pos.y = 4.5f;
+        }
+
+        transform.position = pos;
     }
 }
