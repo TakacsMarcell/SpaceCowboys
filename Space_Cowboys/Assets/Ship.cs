e2c2b5291fb96a89c927eeb6e41201e47ad21083
fzksbalazs <70636449+fzksbalazs@users.noreply.github.com>
2024-12-03 21:55:02 +0100

    Fegyver hozzáadása

--- Space_Cowboys/Assets/Ship.cs
+++ Space_Cowboys/Assets/Ship.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
 
+    Gun[] guns;
+
     float moveSpeed = 3;
     bool moveUp;
     bool moveDown;
@@ -12,10 +14,12 @@ public class Ship : MonoBehaviour
     bool moveRight;
     bool speedUp;
 
+    bool shoot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        guns = transform.GetComponentsInChildren<Gun>();
     }
 
     // Update is called once per frame
@@ -26,6 +30,17 @@ public class Ship : MonoBehaviour
         moveLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
         moveRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
         speedUp = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+
+        shoot = Input.GetKeyDown(KeyCode.LeftControl);
+        if(shoot)
+        {
+            shoot = false;
+            foreach(Gun gun in guns)
+            {
+                gun.Shoot();
+            }
+        }
     }
     private void FixedUpdate()
     {
